1a8fa6203c7e5d604257f5b1b232f1043d717140
GingerBeardx <ecryptographer@gmail.com>
2018-07-08 11:25:08 -0600

    added gold counter and gold reset to Ninja Gold assignment

--- Flask_Fundamentals/Assignment_06_Ninja_Gold/server.py
+++ Flask_Fundamentals/Assignment_06_Ninja_Gold/server.py
@@ -1,10 +1,29 @@
 from flask import Flask, render_template, session, redirect, request
 import random
 app = Flask(__name__)
-app.secret_key = 'xv02A56!#%$'
+app.key = random.randrange(0,15678895)
+app.secret_key = str(app.key)
+
+
+def update_total_gold():
+    try:
+        session['total_gold'] += random.randrange(0,51)
+    except KeyError:
+        session['total_gold'] = 0
 
 @app.route('/')
 def index():
+    print session
     return render_template('index.html')
+
+@app.route('/process_money', methods=['POST'])
+def proc_money():
+    update_total_gold()
+    return redirect('/')
+
+@app.route('/reset', methods=['POST'])
+def reset_gold():
+    session['total_gold'] = 0
+    return redirect('/')
 
 app.run(debug=True)
