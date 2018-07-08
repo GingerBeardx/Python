cf1afbbd8b7c655184ba34d1efc4b8f186fee698
GingerBeardx <ecryptographer@gmail.com>
2018-07-08 11:39:00 -0600

    added buildings random gold and added to total gold to Ninja Gold assignment

--- Flask_Fundamentals/Assignment_06_Ninja_Gold/server.py
+++ Flask_Fundamentals/Assignment_06_Ninja_Gold/server.py
@@ -5,20 +5,28 @@ app.key = random.randrange(0,15678895)
 app.secret_key = str(app.key)
 
 
-def update_total_gold():
+def update_total_gold(add_gold):
     try:
-        session['total_gold'] += random.randrange(0,51)
+        session['total_gold'] += add_gold
     except KeyError:
         session['total_gold'] = 0
 
 @app.route('/')
 def index():
-    print session
+    update_total_gold(0)
     return render_template('index.html')
 
 @app.route('/process_money', methods=['POST'])
 def proc_money():
-    update_total_gold()
+    if request.form['building'] == 'farm':
+        add_gold = random.randrange(10, 21)
+    elif request.form['building'] == 'cave':
+        add_gold = random.randrange(5, 11)
+    elif request.form['building'] == 'house':
+        add_gold = random.randrange(2, 6)
+    elif request.form['building'] == 'casino':
+        add_gold = random.randrange(-50, 51)
+    update_total_gold(add_gold)
     return redirect('/')
 
 @app.route('/reset', methods=['POST'])
