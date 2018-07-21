fd5340a6e01a07fe128e94f805575ff89442d912
GingerBeardx <ecryptographer@gmail.com>
2018-07-21 17:33:29 -0600

    Moved validation do its own file and corrected date formatting issue on success page

--- Flask_And_SQL/Assignment_03_Email_Validation/server.py
+++ Flask_And_SQL/Assignment_03_Email_Validation/server.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, redirect, render_template, session, flash
 from mysqlconnection import MySQLConnector
-from validate_email import validate_email
+from validate import val_email
 
 app = Flask(__name__)
 app.secret_key = "WeHaveTheMeats"
@@ -13,26 +13,13 @@ def index():
 @app.route('/address', methods=['POST'])
 def address():
     #validate e-mail
-    print request.form['email']
-    errors = False
-
-    #verify an e-mail address was entered
-    if len(request.form['email']) < 1:
-        flash('Please enter your email address', 'danger')
-        errors = True
-    
-    # validate e-mail
-    is_valid = validate_email(request.form['email'])
-    if is_valid == False:
-        flash('Please enter a valid email address', 'danger')
-        errors = True
+    errors = val_email(request.form['email'])
     
     # if validation did not pass
     if errors == True:
         return redirect('/')
     
     # if validation passes
-
     # update the database with new address
     query = "INSERT INTO emails (email, created_at, updated_at) VALUES (:email, NOW(), NOW());"
     data = {
@@ -47,14 +34,13 @@ def address():
 
 @app.route('/success')
 def addresses():
-    query = "SELECT id, email, DATE_FORMAT(created_at, '%m/%d/%Y %l:%m %p') AS time FROM emails"
+    query = "SELECT id, email, DATE_FORMAT(created_at, '%m/%d/%Y %l:%i %p') AS time FROM emails"
     users = mysql.query_db(query)
     return render_template('addresses.html', email_data=users)
 
 @app.route('/delete/<email_id>', methods=['POST'])
 def delete(email_id):
-    email = request.form['email']
-    flash('The email address, {}, has been deleted.'.format(email), 'danger')
+    flash('The email address, {}, has been deleted.'.format(request.form['email']), 'danger')
 
     query = "DELETE FROM emails WHERE id = :id"
     data = {'id': email_id}
