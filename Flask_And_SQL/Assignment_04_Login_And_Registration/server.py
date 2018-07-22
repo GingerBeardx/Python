08c3c290b61666c5e7bb798b6918b3f51a740a1a
GingerBeardx <ecryptographer@gmail.com>
2018-07-22 11:11:12 -0600

    Added password hashing and made HTMl more responsive to user actions

--- Flask_And_SQL/Assignment_04_Login_And_Registration/server.py
+++ Flask_And_SQL/Assignment_04_Login_And_Registration/server.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, redirect, render_template, session, flash
 from mysqlconnection import MySQLConnector
 from validate import validate_registration
+import md5
 
 app = Flask(__name__)
 app.secret_key = "AMessageFromTheKing"
@@ -8,18 +9,27 @@ mysql = MySQLConnector(app, 'login')
 
 @app.route('/')
 def index():
-    if not 'show_button' in session:
-        session['show_button'] = []
+    if not 'show_register' in session:
+        session['show_register'] = []
+    if not 'show_login' in session:
+        session['show_login'] = []
+    if not 'show_logout' in session:
+        session['show_logout'] = []
     if not 'user_name' in session:
         session['user_name'] = []
+    
+    if len(session['user_name']) < 2:
+        session['show_register'] = 'inline-block'
+        session['show_login'] = 'inline-block'
+        session['show_logout'] = 'd-none'
     print "*" * 80
     print session
-    session['show_button'] = 'inline-block'
     return render_template('index.html')
 
 @app.route('/register')
 def register():
-    session['show_button'] = 'd-none'
+    session['show_register'] = 'd-none'
+    session['show_login'] = 'inline-block'
     return render_template('register.html')
 
 @app.route('/registration', methods=['POST'])
@@ -27,27 +37,33 @@ def registration():
     if validate_registration(request.form) == True:
         return redirect('/register')
     #Update database
+    hased_password = md5.new(request.form['password']).hexdigest()
     query = "INSERT INTO users (email, password, first_name, last_name, created_at, updated_at) VALUES (:email, :password, :first_name, :last_name, NOW(), NOW());"
     data = {
         'email': request.form['email'],
-        'password': request.form['password'],
+        'password': hased_password,
         'first_name': request.form['first_name'],
         'last_name': request.form['last_name'],
     }
     mysql.query_db(query, data)
-    flash('Congratulations! User {} {} has been successfully registered'.format(request.form['first_name'], request.form['last_name']), 'success')
+    flash('Congratulations! User {} {} has been successfully registered!'.format(request.form['first_name'], request.form['last_name']), 'success')
     session['user_name'] = '{} {}'.format(request.form['first_name'], request.form['last_name'])
     flash('User: {} has been successfully logged in!'.format(session['user_name']), 'success')
     return redirect('/loggedin')
 
 @app.route('/loggedin')
 def loggedin():
+    session['show_login'] = "d-none"
+    session['show_register'] = "d-none"
+    session['show_logout'] = "inline-block"
     query = "SELECT email, password, first_name, last_name, DATE_FORMAT(created_at, '%m/%d/%Y %l:%i %p') AS time FROM users"
     users = mysql.query_db(query)
     return render_template('dash.html', all_users=users)
 
 @app.route('/login')
 def login():
+    session['show_login'] = 'd-none'
+    session['show_register'] = 'inline-block'
     return render_template('login.html')
 
 @app.route('/logcheck', methods=['POST'])
@@ -61,13 +77,17 @@ def logcheck():
     if user == []:
         flash('User does not exist. Please confirm info or register.', 'danger')
         return redirect('/login')
-    print request.form['password']
-    for info in user:
-        if info['password'] != request.form['password']:
-            flash('Password for user is incorrect.', 'danger')
-            return redirect('/login')
-        session['user_name'] = '{} {}'.format(info['first_name'], info['last_name'])
+    hashed_password = md5.new(request.form['password']).hexdigest()
+    if user[0]['password'] != hashed_password:
+        flash('Password for user is incorrect.', 'danger')
+        return redirect('/login')
+    session['user_name'] = '{} {}'.format(user[0]['first_name'], user[0]['last_name'])
     flash('User: {} has been successfully logged in!'.format(session['user_name']), 'success')
     return redirect('/loggedin')
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect('/')
 
 app.run(debug=True)
