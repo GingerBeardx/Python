75813da6285b8a55f51eb4d3b9493c3f3489b145
GingerBeardx <ecryptographer@gmail.com>
2018-08-04 12:22:28 -0600

    Added Dragon class and completed Assignment 04

--- OOP/Assignment_04_Animal.py
+++ OOP/Assignment_04_Animal.py
@@ -16,14 +16,29 @@ class Animal(object):
 class Dog(Animal):
   def __init__(self, name):
     super(Dog, self).__init__(name)
-    self.name = name
     self.health = 150
   def pet(self):
     self.health += 5
     return self
+
+class Dragon(Animal):
+  def __init__(self, name):
+    super(Dragon, self).__init__(name)
+    self.health = 170
+  def fly(self):
+    self.health -= 10
+    return self
+  def display_health(self):
+    print "I am a Dragon"
+    super(Dragon, self).display_health()
+    return "******************************"
+
 
 animal1 = Animal("Fred")
 print animal1.walk().walk().walk().run().run().display_health()
 
 animal2 = Dog("Stitch")
 print animal2.walk().walk().walk().run().run().pet().display_health()
+
+anmial3 = Dragon("Balthazor")
+print anmial3.fly().display_health()
